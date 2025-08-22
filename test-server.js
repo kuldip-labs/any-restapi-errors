e9125bf344bf25077b3630e3a754f1d84a342f86
Kuldipkumar Prajapati <Kuldipgithub@outlook.com>
2025-08-22 13:27:49 +0530

    is operational

--- test-server.js
+++ test-server.js
@@ -1,9 +1,9 @@
-import  express from 'express';
-import  {apiError} from './index.js'; // Assuming api404Error is a custom error handler
+import express from 'express';
+import { apiError } from './index.js'; // Assuming api404Error is a custom error handler
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/error', (req, res) => {
   const error = new Error("An error message")
   console.log(error.message)
   console.log(error.stack)
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
   res.send(error)
 })
 
-app.get('/error', (req, res) => {   
+app.get('/apiError', (req, res) => {
   // Simulating an error    
   try {
     throw new apiError(404);
@@ -20,9 +20,26 @@ app.get('/error', (req, res) => {
     console.error(error.description);
     // Log the error using the custom error logger
     error.errorLogger();
+    if (!error.isOperationalError(error)) {
+      process.exit(1)
+    }
     res.status(error.statusCode || 500).send(error.resObj());
   }
-  
+
+})
+
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception:', error);
+  if (!error.isOperational) {
+    // log it and exit the process
+    process.exit(1);
+  }
+})
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  // log it, exit the process
+  process.exit(1);
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
